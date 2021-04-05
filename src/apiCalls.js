aa5fb52208acab89483452eef063828e040a7d4d
flashbackditto <chaunceyberkowits@yahoo.com>
2021-04-05 14:24:48 -0700

    git problems test

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -37,13 +37,14 @@ render() {
     return <div>Loading...</div>;
   } else {
     return (
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>
-            {item.name} {item.price}
-          </li>
-        ))}
-      </ul>
+      console.log(items);
+      // <ul>
+      //   {items.map(item => (
+      //     <li key={item.id}>
+      //       {item.name} {item.price}
+      //     </li>
+      //   ))}
+      // </ul>
     );
   }
 }
